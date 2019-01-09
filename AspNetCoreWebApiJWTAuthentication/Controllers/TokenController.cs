e75c7ae73b939d43751a0c0c351c42ce2f5f7303
毕殿卿 <bdq123@outlook.com>
2019-01-10 00:28:36 +0800

    *

--- AspNetCoreWebApiJWTAuthentication/Controllers/TokenController.cs
+++ AspNetCoreWebApiJWTAuthentication/Controllers/TokenController.cs
@@ -47,11 +47,11 @@ namespace AspNetCoreWebApiJWTAuthentication.Controllers
 
                 var jwtSecurityToken = new JwtSecurityToken
                 (
-                    issuer: _configuration["Issuer"],                   // 发行人
-                    audience: _configuration["Audience"],               // 接收方
-                    claims: claims,                                     // 声明
-                    expires: DateTime.Now.AddSeconds(30),               // 过期时间
-                    notBefore: DateTime.Now,                            // 定义在什么时间之前，该jwt都是不可用的
+                    issuer: _configuration["Issuer"],                       // 发行人
+                    audience: _configuration["Audience"],                   // 接收方
+                    claims: claims,                                         // 声明
+                    expires: DateTime.Now.AddDays(30).Date.AddHours(4),     // 过期时间 只在凌晨4点过期
+                    notBefore: DateTime.Now,                                // 定义在什么时间之前，该jwt都是不可用的
                     signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SigningKey"])),
                          SecurityAlgorithms.HmacSha256)
                 );
